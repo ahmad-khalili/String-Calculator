e4adcf2aaf450b60a9ccd801919867e6d2452dc2
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 14:50:56 +0300

    Addition: Add a failing test for comma separated additions

--- Calculator.Tests/CommaSeparatedCalculatorTests.cs
+++ Calculator.Tests/CommaSeparatedCalculatorTests.cs
@@ -1,6 +1,18 @@
-﻿namespace Calculator.Tests;
+﻿using Calculator.Calculators;
+
+namespace Calculator.Tests;
 
 public class CommaSeparatedCalculatorTests
 {
-    
+    [Theory]
+    [InlineData("0,0", 0)]
+    [InlineData("1,2,3", 6)]
+    public void CommaSeparatedAdditionTest_ShouldReturnCorrespondingExpectedResult(string numbers, decimal expected)
+    {
+        var commaSeparatedCalculator = new CommaSeperatedCalculator();
+
+        var actual = commaSeparatedCalculator.CommaSeperatedAdd(numbers);
+
+        actual.Should().Be(expected);
+    }
 }
