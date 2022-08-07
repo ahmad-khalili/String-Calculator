7d9b7b841334818191115fb7e855b086357956f0
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 14:55:02 +0300

    Addition: Implement method to sum a comma separated number string

--- Calculator/Calculators/CommaSeperatedCalculator.cs
+++ Calculator/Calculators/CommaSeperatedCalculator.cs
@@ -4,6 +4,13 @@ public class CommaSeperatedCalculator : ICommaSeperatedCalculator
 {
     public decimal CommaSeperatedAdd(string numbers)
     {
-        throw new NotImplementedException();
+        var numbersArray = numbers.Split(',');
+        decimal result = 0;
+
+        if (numbersArray.Any())
+            foreach (var number in numbersArray)
+                result += decimal.Parse(number);
+
+        return result;
     }
 }
