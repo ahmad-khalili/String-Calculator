31d447c6ac65f6d0ab82a148535f776e24c822d1
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 14:27:30 +0300

    Addition: Initialize comma seperated object in StringCalculator

--- Calculator/Calculators/StringCalculator.cs
+++ Calculator/Calculators/StringCalculator.cs
@@ -2,6 +2,13 @@ namespace Calculator.Calculators;
 
 public class StringCalculator
 {
+    private readonly ICommaSeperatedCalculator _commaSeperatedCalculator;
+
+    public StringCalculator(ICommaSeperatedCalculator commaSeperatedCalculator)
+    {
+        _commaSeperatedCalculator = commaSeperatedCalculator;
+    }
+    
     public decimal Add(string numbers)
     {
         if (string.IsNullOrEmpty(numbers)) return 0;
