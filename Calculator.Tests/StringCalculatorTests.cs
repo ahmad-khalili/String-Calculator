5d0b17545d40658d8b1128a4d68699fe9102a1ef
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 13:31:31 +0300

    Addition: Write a failing test for empty strings

---
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Calculator.Tests;
+
+public class StringCalculatorTests
+{
+    [Fact]
+    public void EmptyNumbersStringAdditionTest_ShouldReturnZero()
+    {
+        var stringCalculator = new StringCalculator();
+        var expected = 0;
+        var testString = "";
+
+        var actual = stringCalculator.Add(testString);
+
+        actual.Should().Be(expected);
+    }
+}
