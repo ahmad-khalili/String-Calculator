2d9f01402ea604b3cbe798114571a57516b2f32b
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 15:03:00 +0300

    Refactoring: Move calculator object initialization to constructor

--- Calculator.Tests/CommaSeparatedCalculatorTests.cs
+++ Calculator.Tests/CommaSeparatedCalculatorTests.cs
@@ -4,14 +4,19 @@ namespace Calculator.Tests;
 
 public class CommaSeparatedCalculatorTests
 {
+    private readonly CommaSeparatedCalculator _commaSeparatedCalculator;
+
+    public CommaSeparatedCalculatorTests()
+    {
+        _commaSeparatedCalculator = new CommaSeparatedCalculator();
+    }
+    
     [Theory]
     [InlineData("0,0", 0)]
     [InlineData("1,2,3", 6)]
     public void CommaSeparatedAdditionTest_ShouldReturnCorrespondingExpectedResult(string numbers, decimal expected)
     {
-        var commaSeparatedCalculator = new CommaSeperatedCalculator();
-
-        var actual = commaSeparatedCalculator.CommaSeperatedAdd(numbers);
+        var actual = _commaSeparatedCalculator.CommaSeperatedAdd(numbers);
 
         actual.Should().Be(expected);
     }
