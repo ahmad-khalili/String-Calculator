8f216e3f34892c9841ff892b7a313922b5600bf0
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-07 13:41:52 +0300

    Addition: Add a failing test with a 1 number string

--- Calculator.Tests/StringCalculatorTests.cs
+++ Calculator.Tests/StringCalculatorTests.cs
@@ -15,4 +15,16 @@ public class StringCalculatorTests
 
         actual.Should().Be(expected);
     }
+
+    [Fact]
+    public void OneNumberStringAdditionTest_ShouldReturnNumber()
+    {
+        var stringCalculator = new StringCalculator();
+        var expected = 1;
+        var testString = "1";
+
+        var actual = stringCalculator.Add(testString);
+
+        actual.Should().Be(expected);
+    }
 }
