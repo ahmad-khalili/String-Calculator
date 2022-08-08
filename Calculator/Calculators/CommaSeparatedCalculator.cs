403d798fea739ed1a3faacaa8def39263d8ee102
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-08 17:44:10 +0300

    Delimiters: Implement actual delimiter handling

--- Calculator/Calculators/CommaSeparatedCalculator.cs
+++ Calculator/Calculators/CommaSeparatedCalculator.cs
@@ -1,15 +1,24 @@
-﻿namespace Calculator.Calculators;
+﻿using Calculator.Constants;
+
+namespace Calculator.Calculators;
 
 public class CommaSeparatedCalculator : ICommaSeparatedCalculator
 {
     public decimal CommaNewLineSeperatedAdd(string numbers)
     {
-        var numbersArray = numbers.Split(',', '\n');
+        numbers = numbers.TrimStart('/');
+        var numbersArray = numbers.Split(StringConstants.Splitters.ToArray());
         decimal result = 0;
 
         if (numbersArray.Any())
+        {
+            numbersArray = numbersArray.Where(x => !string.IsNullOrWhiteSpace(x)).ToArray();
             foreach (var number in numbersArray)
+            {
+                Console.WriteLine(number);
                 result += decimal.Parse(number);
+            }
+        }
 
         return result;
     }
