1017fef306133de90e1811fd1334510f2c577b54
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-08 17:52:27 +0300

    Delimiters: Add DelimiterOperator unit tests

---
+++ Calculator.Tests/DelimiterOperatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Calculator.Constants;
+using Calculator.Operators;
+
+namespace Calculator.Tests;
+
+public class DelimiterOperatorTests
+{
+    private readonly DelimiterOperator _delimiterOperator;
+
+    public DelimiterOperatorTests()
+    {
+        _delimiterOperator = new DelimiterOperator();
+    }
+
+    [Theory]
+    [InlineData("//;\n1;2",';')]
+    public void GetDelimiterTest_ShouldReturnCorrespondingExpectedValue(string numbers, char expected)
+    {
+        var actual = _delimiterOperator.GetDelimiter(numbers);
+
+        actual.Should().Be(expected);
+    }
+}
