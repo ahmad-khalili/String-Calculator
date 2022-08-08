7ed3ef633f8bd4526021be11d3b3f23fb8a30936
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-08 16:36:27 +0300

    Delimiters: Add a class to handle delimiter processing

---
+++ Calculator/Operators/StringOperator.cs
@@ -0,0 +1,20 @@
+﻿using Calculator.Constants;
+
+namespace Calculator.Operators;
+
+public class StringOperator
+{
+    public char GetDelimiter(string numbers)
+    {
+        var delimiterPrefix = "//";
+        if (numbers.StartsWith(delimiterPrefix))
+        {
+            var delimiterPrefixIndex = 1;
+            var delimiter = char.Parse(numbers.Substring(delimiterPrefixIndex + 1));
+            StringConstants.Splitters.Add(delimiter);
+            return delimiter;
+        }
+
+        return default;
+    }
+}
