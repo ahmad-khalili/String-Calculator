7132291e867069bd79d3b566da797f228baa09d1
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-09 17:14:32 +0300

    Negatives: Implement Actual GetNegatives method

--- Calculator/Operators/NegativeFinder.cs
+++ Calculator/Operators/NegativeFinder.cs
@@ -6,6 +6,13 @@ public class NegativeFinder : INegativeFinder
 {
     public List<string> GetNegatives(string numbers)
     {
-        throw new NotImplementedException();
+        var numbersArray = numbers.Split(StringConstants.Splitters.ToArray());
+        var negativesList = new List<string>();
+        
+        foreach (var number in numbersArray)
+            if (number.Contains('-'))
+                negativesList.Add(number);
+
+        return negativesList;
     }
 }
