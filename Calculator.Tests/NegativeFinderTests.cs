569f60d9acd1b9809f59f022e29266aec64e9646
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-09 17:10:10 +0300

    Negatives: Test NegativeFinder's GetNegatives method

---
+++ Calculator.Tests/NegativeFinderTests.cs
@@ -0,0 +1,24 @@
+﻿using Calculator.Extensions;
+using Calculator.Operators;
+
+namespace Calculator.Tests;
+
+public class NegativeFinderTests
+{
+    private readonly NegativeFinder _negativeFinder;
+
+    public NegativeFinderTests()
+    {
+        _negativeFinder = new NegativeFinder();
+    }
+
+    [Theory]
+    [InlineData("1,4,-1", "-1")]
+    [InlineData("-1,-2,-3,4", "-1, -2, -3")]
+    public void GetNegativesTest_ShouldReturnCorrespondingNegatives(string numbers, string expected)
+    {
+        var actual = _negativeFinder.GetNegatives(numbers).Print();
+
+        actual.Should().Be(expected);
+    }
+}
