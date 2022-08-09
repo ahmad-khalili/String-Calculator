75427e91153b47128a4b9cb8605a5e4ccb1f1941
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-09 16:56:33 +0300

    Negatives: Add negative finder class and interface

---
+++ Calculator/Operators/NegativeFinder.cs
@@ -0,0 +1,11 @@
+﻿using Calculator.Constants;
+
+namespace Calculator.Operators;
+
+public class NegativeFinder : INegativeFinder
+{
+    public List<string> GetNegatives(string numbers)
+    {
+        throw new NotImplementedException();
+    }
+}
