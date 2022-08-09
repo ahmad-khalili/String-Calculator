6ebc5570be359243fa8841d0aeffc6bd24c1dc35
Ahmad Khalili <ahmadkhalili01@outlook.com>
2022-08-09 16:57:19 +0300

    Refactoring: Add list printing extension method

---
+++ Calculator/Extensions/StringListExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator.Extensions;
+
+public static class StringListExtensions
+{
+    public static string Print(this List<string> list)
+    {
+        var str = "";
+        var isFirstLoop = true;
+
+        foreach (var element in list)
+        {
+            if (isFirstLoop)
+            {
+                str += element;
+            }
+            else
+            {
+                str += $", {element}";
+            }
+            isFirstLoop = false;
+        }
+
+        return str;
+    }
+}
